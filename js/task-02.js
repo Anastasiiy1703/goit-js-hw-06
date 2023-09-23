1730e7922a114aac1311f6e88e95e83587b29701
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-09-23 23:12:19 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const container = document.querySelector("#ingredients");
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.className = "item";
+  container.append(li)
+ 
+})
+
+console.log(container);
+
+
+
+
+
+
+
+
+
