4da2053021b80eea305e12103dc00a017b0c4281
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-09-24 16:59:13 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+        if (!email.value || !password.value) {
+            alert("Всі поля повинні бути заповнені!")
+        }
+
+    console.log(data);
+    event.currentTarget.reset();
+}
 
