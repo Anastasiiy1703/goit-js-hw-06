ab47e30fb752ed6bf6a27e82ca5fe8dc7a6a4e54
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-09-24 19:15:13 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const click = document.querySelector(".change-color");
+const bodyElement = document.querySelector("body");
+const span = document.querySelector(".color");
+
+click.addEventListener('click', changeColor);
+function changeColor() {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+span.textContent = getComputedStyle(bodyElement).backgroundColor;
+
+}
+
