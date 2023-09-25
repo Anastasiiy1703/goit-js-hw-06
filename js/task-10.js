145437c9492088d4d2ce155f98b4c90ab8d05b41
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-09-25 20:28:02 +0300

    task10.1

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBoxes = document.querySelector("#boxes");
+
+
+function createBoxes(amount) {
+  const divs = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    document.divBoxes.appendChild(div);
+    divs.push(div);
+  }
+  return divs;
+ console.log(divs);
+}
+
+const createdDivs = createBoxes(3); 
+
+// console.log(createdDivs); 
+
+// console.log(divBoxes);
+    
+
